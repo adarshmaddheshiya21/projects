3e3ac32e9b092e621e2c3a8514c9b43426c1a5a9
Adarsh Maddheshiya <adarshmaddheshiya20@gmail.com>
2023-11-25 18:14:28 +0530

    first commit

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,11 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+
+// routes
+app.get('/hellow', (req, res)=> {
+    res.send('Task Manager App');
+})
+
+app.listen(3000, ()=> {
+    console.log("The server is running on port 3000...");
+})
